ec4a319a18d57ccfb94fe435aabd3d01c70448a3
rfmineguy <rfmineguy@gmail.com>
2022-12-20 18:56:36 -0800

    Modified name of docker image

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker run --rm -it --name tmp -w /root/workspace/ -v ~/Documents/dev/operation-systems/nanobyte-os/:/root/workspace/ os-compilation-img make floppy_image
+docker run --rm -it --name tmp -w /root/workspace/ -v ~/Documents/dev/operation-systems/nanobyte-os/:/root/workspace/ rfmineguy/os-comp-image make floppy_image
 make qemu_run
